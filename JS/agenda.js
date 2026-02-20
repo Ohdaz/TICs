70baa14c6c6ce33eab4f812e64098d3324bd61de
20216558 HAZIEL MURGUIA TORRES <20216558@TELEMATICA.UCOL.MX>
2026-02-20 08:31:40 -0600

    Lista visible

--- JS/agenda.js
+++ JS/agenda.js
@@ -1,39 +1,47 @@
 document.write('Una agenda');
 let seguir = true;
 let datos = [];
+let contactos = [];
 
 while (seguir) {
     let opcion = parseInt(prompt('Escoge una opción \n 1:Agregar \n 2:Listar \n 3:Salir'));
     switch (opcion) {
         case 1:
             alert('Escogiste la opción 1');
+            let id = parseInt(contactos.length + 1);
             let nombre = prompt('Nombre:');
-            let apellidos = prompt('Apellidos:');
-            let facultad = prompt('Facultad:');
             let telefono = parseInt(prompt('Teléfono:'));
             let edad = parseInt(prompt('Edad:'));
             let nuevoContacto = {
+                id: id,
                 nombre: nombre,
-                apellidos: apellidos,
-                facultad: facultad,
                 telefono: telefono,
                 edad: edad
             };
-            datos.push(nuevoContacto)
-            console.table(datos)
+            contactos.push(nuevoContacto);
+            console.table(contactos);
             alert('Contacto agregado con éxito');
             break;
         case 2:
-            if (datos.length === 0) {
+            let lista = "";
+            for (let i = 0; i < contactos.length; i++) {
+                lista += "Id: " + contactos[i].id + "\n";
+                lista += "Nombre: " + contactos[i].nombre + "\n";
+                lista += "Teléfono: " + contactos[i].telefono + "\n";
+                lista += "Edad: " + contactos[i].edad + "\n";
+            }
+            if (contactos.length === 0) {
                 alert('La agenda está vacía');
             } else {
-                alert('Escogiste la opción 2');
+                alert(lista);
             }
             break;
         case 3:
             alert('Escogiste salir');
             seguir = false;
             break;
+
+        //case 4:
         default:
             alert('No recibí una opción válida')
     }
